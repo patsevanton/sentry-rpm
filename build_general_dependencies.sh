1b083d9dcbcdd4fe50ba5c73df9d4ce1541cfdd7
Anton Patsev <patsev.anton@gmail.com>
2020-05-01 15:45:51 +0600

    python2-pip

--- build_general_dependencies.sh
+++ build_general_dependencies.sh
@@ -6,7 +6,7 @@ sudo yum install -y epel-release git
 echo "Install dependencies"
 sudo yum install -y cargo gcc gcc-c++ libffi-devel libjpeg-devel libxml2-devel \
 libxslt libxslt-devel make mc openssl-devel postgresql-devel python-devel \
-python-lxml python-nose python3-pip python34 rpm-build rpmdevtools \
+python-lxml python-nose python2-pip python34 rpm-build rpmdevtools \
 ruby-devel rubygems zlib-devel
 
 echo "Install fpm"
