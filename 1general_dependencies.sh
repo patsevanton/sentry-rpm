eecb20eb0207d3d4886399dc773b6b78b70073af
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-05-02 18:48:21 +0600

    Update 1general_dependencies.sh

--- 1general_dependencies.sh
+++ 1general_dependencies.sh
@@ -7,7 +7,7 @@ echo "Install dependencies"
 sudo yum install -y cargo gcc gcc-c++ libffi-devel libjpeg-devel libxml2-devel \
 libxslt libxslt-devel make mc openssl-devel postgresql-devel python-devel \
 python-lxml python-nose python2-pip python34 rpm-build rpmdevtools \
-ruby-devel rubygems zlib-devel redis
+ruby-devel rubygems zlib-devel redis xmlsec1-openssl xmlsec1
 
 echo "Install fpm"
 gem install --no-document fpm
