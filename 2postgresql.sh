4b20018eb2332587717c78451625b36b727715ab
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-05-03 11:41:09 +0600

    Update 2postgresql.sh

--- 2postgresql.sh
+++ 2postgresql.sh
@@ -2,3 +2,12 @@
 
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 yum install -y postgresql96 postgresql96-server postgresql96-contrib postgresql96-devel
+/usr/pgsql-9.6/bin/postgresql96-setup initdb
+systemctl start postgresql-9.6
+create user sentry with password 'password';
+create database sentry with owner sentry;
+alter role sentry superuser;
+
+
+#alter role sentry nosuperuser;
+#CREATE SCHEMA main AUTHORIZATION sentry;
