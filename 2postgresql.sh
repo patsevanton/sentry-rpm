9f74377de57293d8e7c8e76662af50c057fbabbc
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-05-03 14:38:48 +0600

    Update 2postgresql.sh

--- 2postgresql.sh
+++ 2postgresql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-yum install -y postgresql96-devel
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo yum install -y postgresql96-devel
