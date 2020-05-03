e0ba85a8cb2d75143c6ede6887ee2b399aa404cc
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-05-03 17:03:17 +0600

    Update 9sentry-ldap-auth.sh

--- 9sentry-ldap-auth.sh
+++ 9sentry-ldap-auth.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+echo "Build sentry-ldap-auth rpm"
+spectool -g -R spec/sentry-ldap-auth-2.8.1.spec
+sudo yum-builddep -y spec/sentry-ldap-auth-2.8.1.spec
+rpmbuild --bb spec/sentry-ldap-auth-2.8.1.spec
+sudo yum install -y ~/rpmbuild/RPMS/x86_64/python2-sentry-ldap-auth-2.8.1
