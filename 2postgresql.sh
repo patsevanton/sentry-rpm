60ce66ccf2fe0af80423e7c2461ba38f1b4a2e8f
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-05-03 11:43:33 +0600

    Update 2postgresql.sh

--- 2postgresql.sh
+++ 2postgresql.sh
@@ -4,10 +4,9 @@ yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_6
 yum install -y postgresql96 postgresql96-server postgresql96-contrib postgresql96-devel
 /usr/pgsql-9.6/bin/postgresql96-setup initdb
 systemctl start postgresql-9.6
-create user sentry with password 'password';
-create database sentry with owner sentry;
-alter role sentry superuser;
+sudo -i -u postgres psql -c \"create user sentry with password 'password';\"
+sudo -i -u postgres psql -c \"create database sentry with owner sentry;\"
+sudo -i -u postgres psql -c \"alter role sentry superuser;\"
 
-
-#alter role sentry nosuperuser;
-#CREATE SCHEMA main AUTHORIZATION sentry;
+#sudo -i -u postgres psql -c \"alter role sentry nosuperuser;\"
+#sudo -i -u postgres psql -c \"CREATE SCHEMA main AUTHORIZATION sentry;\"
