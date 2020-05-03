fe44d81de8324d61f80c53b525b4def585cc7806
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-05-03 18:29:31 +0600

    Update 2psycopg2-binary.sh

--- 2psycopg2-binary.sh
+++ 2psycopg2-binary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y postgresql-devel
+sudo yum install -y postgresql-devel
 fpm -s python -t rpm psycopg2-binary==2.7.7
 sudo yum install -y python-psycopg2-binary-2.7.7-1.x86_64.rpm
 yum remove -y postgresql-devel
