b8ef181210dbc6be769a55420c32846d62d2afb6
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-05-04 09:56:43 +0600

    Update 8start_sentry.sh

--- 8start_sentry.sh
+++ 8start_sentry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo -i -u sentry --config /etc/sentry/ upgrade
+sudo -i -u sentry /usr/bin/sentry --config /etc/sentry/ upgrade
 sudo systemctl start sentry-worker
 sudo systemctl start sentry-cron
 sudo systemctl start sentry-web
