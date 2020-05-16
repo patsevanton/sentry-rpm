8712955ad40bce43430c7fef7e3e2e420e445705
Anton Patsev <patsev.anton@gmail.com>
2020-05-16 13:02:04 +0600

    relocate python-dateutil-system-zoneinfo.patch

--- 3dateutil.sh
+++ 3dateutil.sh
@@ -6,8 +6,8 @@ sudo yum install -y python-six-1.10.0-1.noarch.rpm
 
 echo "Build and install python-dateutil rpm"
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SRPMS,SPECS}
+cp spec/python-dateutil-system-zoneinfo.patch ~/rpmbuild/SOURCES
+cp spec/python-dateutil-timelex-string.patch ~/rpmbuild/SOURCES
 spectool -g -R spec/python-dateutil.spec
-wget https://raw.githubusercontent.com/patsevanton/sentry-rpm/master/spec/python-dateutil-system-zoneinfo.patch -P ~/rpmbuild/SOURCES
-wget https://raw.githubusercontent.com/patsevanton/sentry-rpm/master/spec/python-dateutil-timelex-string.patch -P ~/rpmbuild/SOURCES
 rpmbuild -bb spec/python-dateutil.spec
 sudo yum install -y ~/rpmbuild/RPMS/noarch/python-dateutil-2.4.2-1.el7.noarch.rpm
